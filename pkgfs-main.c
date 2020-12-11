2d1b90f31d43c1146b4bc8bd785b0371fceb9ccf
Andrew Chambers <ac@acha.ninja>
2020-12-11 21:48:36 +1300

    Be even more strict.

--- pkgfs-main.c
+++ pkgfs-main.c
@@ -147,7 +147,7 @@ static int pkgfs_open(const char *path, struct fuse_file_info *fi) {
   struct hashtablekey k;
   struct idxent *ent;
 
-  if (fi->flags & (O_CREAT|O_APPEND|O_TRUNC|O_TMPFILE))
+  if (fi->flags & (O_WRONLY|O_RDWR|O_CREAT|O_APPEND|O_TRUNC|O_TMPFILE))
     return -EPERM;
 
   htabkey(&k, path, strlen(path));
